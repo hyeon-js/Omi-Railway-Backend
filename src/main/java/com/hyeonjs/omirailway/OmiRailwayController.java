9a940f354c1fea9bb3f41a40fc0befd530046769
h.js <121519834+hyeon-js@users.noreply.github.com>
2025-03-20 02:26:22 +0900

    parameter receiving test

--- src/main/java/com/hyeonjs/omirailway/OmiRailwayController.java
+++ src/main/java/com/hyeonjs/omirailway/OmiRailwayController.java
@@ -2,13 +2,23 @@ package com.hyeonjs.omirailway;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class OmiRailwayController {
 
-    @GetMapping("/")
-    public String getHelloWorld() {
-        return "Hello World";
+    @GetMapping("/omi")
+    public String getRunningInfo(@RequestParam(required = false) String line) {
+        if (line == null) return "{\"error\":\"필수 파라미터 누락\"}";
+        switch(line) {
+            case "honsen":
+                return "[\"본선\"]";
+            case "yokaichi":
+                return "[\"요카이치선\"]";
+            case "taga":
+                return "[\"타가선\"]";
+        }
+        return "{\"error\":\"올바르지 않은 파라미터 값\"}";
     }
 
 }
