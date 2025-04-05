7f9e4c8a8e666cb27f8194d52acab5135051b610
h.js <121519834+hyeon-js@users.noreply.github.com>
2025-04-06 05:03:24 +0900

    modified

--- src/main/java/com/hyeonjs/omirailway/Station.java
+++ src/main/java/com/hyeonjs/omirailway/Station.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Station {
 
 	private String station;
-	private List<Train> up, down;
+	public List<Train> up, down;
 
 	public Station(String station, List<Train> up, List<Train> down) {
 		this.station = station;
@@ -17,15 +17,16 @@ public class Station {
 		return station;
 	}
 	
-	public List<Train> getUp() {
-		return up;
-	}
-	public List<Train> getDown() {
-		return down;
-	}
-
 	public static class Train {
-		private String trainNo, terminal, type;
+
+		private final String trainNo, terminal, type;
+
+		public Train(String trainNo, String terminal) {
+			this.trainNo = trainNo;
+			this.terminal = terminal;
+			if (trainNo.equals("50")) this.type = "쾌속";
+			else this.type = "보통";
+		}
 
 		public String getTrainNo() {
 			return trainNo;
--- src/main/java/com/hyeonjs/omirailway/TrainTimeTable.java
+++ src/main/java/com/hyeonjs/omirailway/TrainTimeTable.java
@@ -1,14 +1,15 @@
 package com.hyeonjs.omirailway;
 
 public class TrainTimeTable {
-    final public String trainNo;
+    final public String trainNo, terminal;
     final public Time[] data;
 
-    public TrainTimeTable(String trainNo, Time[] data) {
+    public TrainTimeTable(String trainNo, Time[] data, String terminal) {
         this.trainNo = trainNo;
         this.data = data;
+        if (terminal != null) this.terminal = terminal;
+        else this.terminal = data[data.length - 1].stn;
     }
-
 
     public static class Time {
         final public String stn, tm;
@@ -22,7 +23,9 @@ public class TrainTimeTable {
 
         private int t2s(String time) {
             String[] t = time.split(":");
-            return 60*Integer.parseInt(t[0]) + Integer.parseInt(t[1]);
+            int h = Integer.parseInt(t[0]);
+            if (h == 0) h = 24;
+            return 60*h + Integer.parseInt(t[1]);
         }
     
     }
